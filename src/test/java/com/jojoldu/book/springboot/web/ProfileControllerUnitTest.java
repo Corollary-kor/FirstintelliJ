5b2c81eb49f59cf2f2b38f33982707f52e8e7e24
alice <ldshoo@naver.com>
2022-02-10 17:52:15 +0900

    complete2

--- src/test/java/com/jojoldu/book/springboot/web/ProfileControllerUnitTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/ProfileControllerUnitTest.java
@@ -46,7 +46,6 @@ public class ProfileControllerUnitTest {
     @Test
     public void active_profile이_없으면_default가_조회된다() {
         //given
-        String expectedProfile = "default";
         MockEnvironment env = new MockEnvironment();
         ProfileController controller = new ProfileController(env);
 
@@ -54,7 +53,6 @@ public class ProfileControllerUnitTest {
         String profile = controller.profile();
 
         //then
-        assertThat(profile).isEqualTo(expectedProfile);
     }
 }
 
